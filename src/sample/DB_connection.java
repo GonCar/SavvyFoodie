d7baf3ca74afc8ba1bebcf2e21cca7de9815846c
Meron Habtemichael <meronmkl@gmail.com>
2021-04-27 23:59:01 +0200

    initialized User, Products and DB_connection classes.

--- src/sample/DB_connection.java
+++ src/sample/DB_connection.java
@@ -1,4 +1,29 @@
 package sample;
+import java.sql.*;
 
 public class DB_connection {
+    String url = "jdbc:mysql://127.0.0.1:3306/classicmodels?user=root&password=root";
+    Connection connection;
+    PreparedStatement ps;
+    ResultSet resultSet;
+    public void connect() {
+        try{
+            connection = DriverManager.getConnection(url);
+        }
+        catch (SQLException ex){
+            System.out.println("connection failed!");
+        }
+    }
+
+    public void disconnect() {
+        try{
+            if(connection != null){ connection.close();}
+            if(ps != null){ ps.close();}
+            if(resultSet != null){ resultSet.close();}
+        }
+        catch (SQLException ex){
+            System.out.println("closing the resources failed!");
+        }
+    }
+
 }
--- src/sample/Products.java
+++ src/sample/Products.java
@@ -1,4 +1,47 @@
 package sample;
 
+import java.util.Date;
+
 public class Products {
+    String product_name;
+    String category;
+    String description;
+    boolean is_veggie;
+    boolean is_gluten_free;
+    int product_id;
+    int product_weight;
+    int price;
+    Date expiry_date;
+
+    public Products(int product_id, String product_name, String category, boolean is_veggie, boolean is_gluten_free, int product_weight, int price, Date expiry_date) {
+        this.product_name = product_name;
+        this.category = category;
+        this.is_veggie = is_veggie;
+        this.is_gluten_free = is_gluten_free;
+        this.product_id = product_id;
+        this.product_weight = product_weight;
+        this.price = price;
+        this.expiry_date = expiry_date;
+    }
+
+    //setters
+    public String getProduct_name() { return product_name; }
+    public void setProduct_name(String product_name) { this.product_name = product_name; }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public boolean isIs_veggie() { return is_veggie; }
+    public void setIs_veggie(boolean is_veggie) { this.is_veggie = is_veggie; }
+    public boolean isIs_gluten_free() { return is_gluten_free; }
+    public void setIs_gluten_free(boolean is_gluten_free) { this.is_gluten_free = is_gluten_free; }
+    // getters
+    public int getProduct_id() { return product_id; }
+    public void setProduct_id(int product_id) { this.product_id = product_id; }
+    public int getProduct_weight() { return product_weight; }
+    public void setProduct_weight(int product_weight) { this.product_weight = product_weight; }
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+    public Date getExpiry_date() { return expiry_date; }
+    public void setExpiry_date(Date expiry_date) { this.expiry_date = expiry_date; }
 }
