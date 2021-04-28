76ed757f0000db3401889c015b215128438f89fe
Gonzalo <walltocc@gmail.com>
2021-04-29 00:04:08 +0200

    show, add, delete to db

--- src/sample/DB_connection.java
+++ src/sample/DB_connection.java
@@ -1,11 +1,14 @@
 package sample;
 import java.sql.*;
+import java.util.Date;
 
 public class DB_connection {
     String url = "jdbc:mysql://127.0.0.1:3306/classicmodels?user=root&password=root";
     Connection connection;
     PreparedStatement ps;
+    Statement statement;
     ResultSet resultSet;
+    ResultSetMetaData rsmd;
     public void connect() {
         try{
             connection = DriverManager.getConnection(url);
@@ -23,6 +26,65 @@ public class DB_connection {
         }
         catch (SQLException ex){
             System.out.println("closing the resources failed!");
+        }
+    }
+
+    public void showProducts()
+    {
+        try{
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("Select * from products");
+            rsmd = resultSet.getMetaData();
+
+            int columnsNumber = rsmd.getColumnCount();
+            while (resultSet.next()) {
+                for(int i = 1 ; i <= columnsNumber; i++){
+                    System.out.print("|" + resultSet.getString(i) + "| ");
+                }
+                System.out.println();
+            }
+
+        }catch (SQLException exception){
+            System.out.println("Query failed to execute");
+            exception.printStackTrace();
+        }
+    }
+
+    public void addProduct(String product_name, String category, boolean is_veggie, boolean is_gluten_free, int product_weight, int price, Date expiry_date)
+    {
+        try {
+
+            statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO products" + "VALUES (" + product_name +","+ category +"," + is_veggie +"," + is_gluten_free +"," + product_weight +","+ price+","+ expiry_date +")");
+
+        }catch (SQLException exception)
+        {
+
+            System.out.println("Could not add product to database");
+            exception.printStackTrace();
+
+        }
+    }
+
+    public void deleteProduct(String productId)
+    {
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SET FOREIGN_KEY_CHECKS=0;");
+
+            statement = connection.createStatement();
+
+            int row = statement.executeUpdate("Delete from products WHERE product_id= '"+ productId + "'");
+
+            System.out.println("The customer was successfully deleted.\n");
+            System.out.println(row + " affected");
+
+        }catch (SQLException exception)
+        {
+
+            System.out.println("Product couldn't be deleted");
+            exception.printStackTrace();
+
         }
     }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,6 +18,13 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
+
+        DB_connection connection = new DB_connection();
+        connection.connect();
+        connection.showProducts();
+
+
     }
 }
