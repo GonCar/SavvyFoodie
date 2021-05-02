a841ec130fd149c1ca6de218b2d9069ba36e72ee
Meron Habtemichael <meronmkl@gmail.com>
2021-05-02 21:03:57 +0200

    implemented changable items(price and username), created app_Logic and GUI classes.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,6 @@
 package sample;
 
 public class Main {
-    public static void main(String[] args) {
-        DB_connection connection = new DB_connection();
-        connection.connect();
-        //connection.filter_by_price( 10, 1000);
-        //connection.filter_by_category("Fruit");
-        connection.filter_by_city("Stockholm");
-        //connection.showProducts();
-    }
+
+
 }
---
+++ src/sample/app_Logic.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class app_Logic {
+    public static void main(String[] args) {
+        DB_connection connection = new DB_connection();
+        connection.connect();
+        //connection.filter_by_price( 10, 1000);
+        //connection.filter_by_category("Fruit");
+        //connection.filter_by_city("Stockholm");
+        //connection.showProducts();
+        //connection.change_price(102,22);
+        //connection.change_user_name(8001, "Richard");
+        connection.disconnect();
+    }
+
+}
