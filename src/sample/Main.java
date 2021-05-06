c652e91ba8964469680899672cfb58d8b22a4953
meronmikel <meronmkl@gmail.com>
2021-05-06 14:29:21 +0200

    moved code from main to app logic

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,11 +2,5 @@ package sample;
 
 public class Main {
     public static void main(String[] args) {
-        DB_connection connection = new DB_connection(new FoodieConnection());
-        connection.connect();
-        //connection.filter_by_price( 10, 1000);
-        //connection.filter_by_category("Fruit");
-        connection.filter_by_city("Stockholm");
-        //connection.showProducts();
     }
 }
