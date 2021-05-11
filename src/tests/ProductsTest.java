386c29f3a6f7d5a975e229480b38795053f10f14
Geng <geng.yuan0002@stud.hkr.se>
2021-05-11 22:39:49 +0200

    Unit Testing for products.java updated. Tests passed!

--- src/tests/ProductsTest.java
+++ src/tests/ProductsTest.java
@@ -12,42 +12,44 @@ class ProductsTest {
 
     @Test
     void getProduct_id(){
-        System.out.println("Product ID test passed");
-        Products test = new Products(0, "", "", true, true, 0, 0, "");
-        Assertions.assertEquals( 0, test.getProduct_id());
+        System.out.println("Product Name test passed");
+        Products test = new Products("apple", "", true, true, 0, 120);
+        Assertions.assertEquals( "apple", test.getProduct_name());
 
     }
 
     @Test
     void getProduct_name(){
-        System.out.println("Product Name test passed");
-        Products test = new Products(0, "apple", "", true, true, 0, 0, "");
-        Assertions.assertEquals("apple", test.getProduct_name());
+        System.out.println("Product Category test passed");
+        Products test = new Products("", "Fruit", true, true, 0, 120);
+        Assertions.assertEquals("Fruit", test.getCategory());
     }
 
 
    @Test
     void is_veggieTrue(){
        System.out.println("Boolean Veggie('true/false') test passed");
-        Products test = new Products(0, "", "", true, true, 0, 0, "");
+        Products test = new Products("apple", "", true, true, 0, 120);
         Assertions.assertEquals( true, test.isIs_veggie());
     }
 
     @Test
     void getBoolean_isGlutenFree(){
         System.out.println("Boolean Gluten Free('true/false') test passed");
-        Products test = new Products(0, "", "", true, true, 0, 0, "");
-        Assertions.assertEquals( true, test.isIs_gluten_free());
+        Products test = new Products("apple", "", true, false, 0, 120);
+        Assertions.assertEquals( false, test.isIs_gluten_free());
     }
 
     @Test
     void getProduct_weight(){
         System.out.println("Product Weight test passed");
-        Products test = new Products(0, "", "", true, true, 55, 0, "");
-        Assertions.assertEquals(55, test.getProduct_weight());
+        Products test = new Products("apple", "", true, true, 99, 120);
+        Assertions.assertEquals(99, test.getProduct_weight());
     }
 
 
 
 
 }
+
+
