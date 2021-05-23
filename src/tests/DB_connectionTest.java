a9dd55db8800d73183b0726d9ef46d5266ac7577
Felicia-H <bingxueerr@gmail.com>
2021-05-23 13:24:05 +0200

    Replaced more tests to use mockito

--- src/tests/DB_connectionTest.java
+++ src/tests/DB_connectionTest.java
@@ -2,137 +2,21 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 import sample.DB_connection;
+import sample.Products;
 
+import java.sql.Array;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-//
-//public class DB_connectionTest {
-//
-//    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-//    private final PrintStream originalOut = System.out;
-//
-//    // Failed connection should display "connection failed!"
-//    @Test
-//    void failedConnection(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.connect();
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("connection failed!"));
-//        System.setOut(originalOut);
-//    }
-//
-//    // Failed connection should display "closing the resources failed!"
-//    @Test
-//    void testDisconnect(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.disconnect();
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("closing the resources failed!"));
-//        System.setOut(originalOut);
-//    }
-//
-//    /**
-//    @Test
-//    void testFilterPrice(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.filter_by_price(2, 3);
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Error on executing statement!"));
-//        System.setOut(originalOut);
-//    }
-//    @Test
-//    void testChangePrice(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.change_price(5, 6);
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Error on executing statement!"));
-//        System.setOut(originalOut);
-//    }
-//    @Test
-//    void testChangeUserName(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.change_user_name(3,"My");
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Error on executing statement!"));
-//        System.setOut(originalOut);
-//    }
-//    @Test
-//    void testFilterByCategory(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.filter_by_category("gluten free");
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Error on executing statement!"));
-//        System.setOut(originalOut);
-//    }
-//    @Test
-//    void testFilterByCity(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.filter_by_city("Lund");
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Error on executing statement!"));
-//        System.setOut(originalOut);
-//    }
-//     **/
-//    @Test
-//    void testShowProducts(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.showProducts();
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Query failed to execute"));
-//        System.setOut(originalOut);
-//    }
-//    @Test
-//    void testAddProduct(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.addProduct("cola", "drink",false,false,150,10,  new Date());
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Could not add product to database"));
-//        System.setOut(originalOut);
-//    }
-//    @Test
-//    void testDeleteProduct(){
-//        System.setOut(new PrintStream(outContent));
-//        DB_connection dbCon = new DB_connection(new FakeFoodieConnection());
-//        dbCon.deleteProduct("cola");
-//        String result = outContent.toString();
-//        assertEquals(true, result.contains("Product couldn't be deleted"));
-//        System.setOut(originalOut);
-//    }
 
 public class DB_connectionTest {
 
-    /**
-    public static void main(String[] args) {
-        String Url = "jdbc:mysql://127.0.0.1:3306/savvyfoodie?user=root&password=root";
-     try {
-        Class.forName("com.mysql.jdbc.Driver");
-        //    System.out.println("Trying to connect");
-            Connection con = DriverManager.getConnection(Url);
-
-            System.out.println("Connection Established Successfull and the DATABASE NAME IS:"
-                    + con.getMetaData().getDatabaseProductName());
-            Statement stmt = con.createStatement();
-            stmt.executeQuery("select * from user;");
-      } catch (Exception e) {
-            System.out.println("Unable to make connection with DB");
-            e.printStackTrace();
-      }
-
-    }
-     **/
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
@@ -147,6 +31,59 @@ public class DB_connectionTest {
         connectionMock.connect();
         String result = outContent.toString();
         assertEquals(true, result.contains("connection failed!"));
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testDisconnect() throws SQLException {
+
+        DB_connection connectionMock = spy(new DB_connection());
+        when(connectionMock.getConnection()).thenThrow(new SQLException());
+        connectionMock.setConnection(mock(Connection.class));
+
+        System.setOut(new PrintStream(outContent));
+        connectionMock.disconnect();
+        String result = outContent.toString();
+        assertEquals(true, result.contains("closing the resources failed!"));
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testFilterPrice() throws SQLException {
+        DB_connection connectionMock = spy(new DB_connection());
+        when(connectionMock.getConnection()).thenThrow(new SQLException());
+        connectionMock.setConnection(mock(Connection.class));
+
+        System.setOut(new PrintStream(outContent));
+        connectionMock.filter_by_price(2, 3);
+        String result = outContent.toString();
+        assertEquals(true, result.contains("Query failed to execute"));
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testFilterByCategory() throws SQLException{
+        DB_connection connectionMock = spy(new DB_connection());
+        when(connectionMock.getConnection()).thenThrow(new SQLException());
+        connectionMock.setConnection(mock(Connection.class));
+
+        System.setOut(new PrintStream(outContent));
+        connectionMock.filter_by_category("gluten free");
+        String result = outContent.toString();
+        assertEquals(true, result.contains("Query failed to execute"));
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testFilterByCity() throws SQLException {
+        DB_connection connectionMock = spy(new DB_connection());
+        when(connectionMock.getConnection()).thenThrow(new SQLException());
+        connectionMock.setConnection(mock(Connection.class));
+
+        System.setOut(new PrintStream(outContent));
+        connectionMock.filter_by_city("Lund");
+        String result = outContent.toString();
+        assertEquals(true, result.contains("Query failed to execute"));
         System.setOut(originalOut);
     }
 }
